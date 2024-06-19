3376b8fe8fca9d341e856d7c92d9a4b1af883a71
Tales Prudente <87439476+RobinCharles984@users.noreply.github.com>
2024-06-19 17:44:26 -0300

    Final commit

--- Lab/Projeto Final/projeto_final_00/projeto_final_00.ino
+++ Lab/Projeto Final/projeto_final_00/projeto_final_00.ino
@@ -7,6 +7,44 @@
 unsigned int countInt = 0;
 unsigned int countCtc = 0;
 bool ligado = true;
+int valorPot = 0;
+int adc_result;
+
+//Função Inicializa conversão AD
+void ADC_init(void)
+{
+  // Configurando Vref para VCC = 5V
+  ADMUX = (1 << REFS0);
+  ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // configuracao do prescaler de 128
+}
+
+//Função lê conversão AD
+int ADC_read(u8 ch)
+{
+  char i;
+  int ADC_temp = 0; // ADC temporário, para manipular leitura
+  int ADC_read = 0; // ADC_read
+  ch &= 0x07;
+  // Zerar os 3 primeiros bits e manter o resto
+  ADMUX = (ADMUX & 0xF8) | ch;
+  // ADSC (ADC Start Conversion)
+  ADCSRA |= (1 << ADSC); // Faça uma conversão
+  // ADIF (ADC Interrupt Flag) é setada quando o ADC pede interrupção
+  // e resetada quando o vetor de interrupção
+  // é tratado.
+  while (!(ADCSRA & (1 << ADIF))); // Aguarde a conversão do sinal
+
+  for (i = 0; i < 8; i++) // Fazendo a conversão 8 vezes para maior precisão
+  {
+    ADCSRA |= (1 << ADSC); // Faça uma conversão
+    while (!(ADCSRA & (1 << ADIF))); // Aguarde a conversão do sinal
+    ADC_temp = ADCL; // lê o registro ADCL
+    ADC_temp += (ADCH << 8); // lê o registro ADCH
+    ADC_read += ADC_temp; // Acumula o resultado (8 amostras) para média
+  }
+  ADC_read = ADC_read >> 3; // média das 8 amostras ( >> 3 é o mesmo que /8)
+  return ADC_read;
+}
 
 int main(){
   //Configurando GPIO
@@ -31,21 +69,23 @@ int main(){
   TCCR2A |= (1 << COM2A1) | (1 << WGM21) | (1 << WGM20);
   TCCR2B |= (1 << CS20);
 
-  //Configurando Conversor AD
-  DDRC=0; //TODOS OS PINOS DO PORTC COMO ENTRADAS
-  ADMUX = 0;
-  ADMUX |= (1<<MUX1); //SELECIONA A ENTRADA PC2
-  ADMUX |= (1<<REFS0); //TENSÃO DE REFERÊNCIA
-  ADCSRA |= (1<<ADEN); //LIGA O CONVERSOR AD
-  ADCSRA |= (1<<ADPS0) | (1<<ADPS1); //PRESCALER = 8
-  ADCSRA |= (1<<ADSC); //INICIA A CONVERSÃO
-
+  Serial.begin(9600);
+  ADC_init();//Liga conversão AD
   sei();//Ligando interrupções e timers
+  
+  //Loop
   while(true){
-    //while (ADCSRA&(1<<ADSC));
-    //Serial.println (ADC);
-    //OCR2A = map (ADC,0,1023,0,255);
-    //ADCSRA |= (1<<ADSC);
+    adc_result = ADC_read(ADC0D); // lê o valor do ADC0 = PC0
+    _delay_ms(50); // Tempo para troca de canal
+
+    // Mostra o valor na serial
+    Serial.print("ADC0: ");
+    Serial.println(adc_result);
+    Serial.print("Resistor: ");
+    Serial.println(valorPot);
+    _delay_ms(100);
+
+    valorPot = (adc_result * 0.25);
   }
 }
 
@@ -55,7 +95,7 @@ ISR(TIMER0_COMPA_vect){
 
     if(ligado){
       if(countCtc==1){
-        OCR2A = 255;  //Vermelho
+        OCR2A = valorPot;  //Vermelho
         OCR1A = 0; //Verde
         OCR1B = 0; //Amarelo
       }
@@ -63,7 +103,7 @@ ISR(TIMER0_COMPA_vect){
       //Troca pro Verde ligado
       if(countCtc==2000){
         OCR2A = 0;  //Vermelho
-        OCR1A = 255; //Verde
+        OCR1A = valorPot; //Verde
         OCR1B = 0; //Amarelo
       }
 
@@ -71,12 +111,12 @@ ISR(TIMER0_COMPA_vect){
       if(countCtc==3500){
         OCR2A = 0;  //Vermelho
         OCR1A = 0; //Verde
-        OCR1B = 255; //Amarelo
+        OCR1B = valorPot; //Amarelo
       }
 
       //Troca pro Vermelho ligado e zera o contador
       if(countCtc==4000){
-        OCR2A = 255;  //Vermelho
+        OCR2A = valorPot;  //Vermelho
         OCR1A = 0; //Verde
         OCR1B = 0; //Amarelo
         countCtc = 0;
@@ -88,7 +128,7 @@ ISR(TIMER0_COMPA_vect){
       if(countCtc==100){
         OCR2A = 0;  //Vermelho
         OCR1A = 0; //Verde
-        OCR1B = 255; //Amarelo
+        OCR1B = valorPot; //Amarelo
       }
 
       if(countCtc==200){
